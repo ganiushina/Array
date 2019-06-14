f6261be57bd9f304e600941a44dc2cae0ad977c9
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-06-14 18:12:09 +0400

    Initial commit

---
+++ src/main/java/Array.java
@@ -0,0 +1,28 @@
+
+
+public interface Array<E> {
+
+    void add(E value);
+
+    E get(int index);
+
+    boolean remove(E value);
+
+    default boolean contains(E value) {
+        return indexOf(value) != -1;
+    }
+
+    int indexOf(E value);
+
+    int size();
+    boolean isEmpty();
+
+    void display();
+
+    long sortBubble();
+    long sortSelect();
+    long sortInsert();
+
+
+
+}
